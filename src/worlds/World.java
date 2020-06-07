f57644680835a16434147ea855f45977ecb32d6f
lucastation <66492964+lucastation@users.noreply.github.com>
2020-06-07 04:44:55 +0300

    Tutorial 18

--- src/worlds/World.java
+++ src/worlds/World.java
@@ -3,10 +3,12 @@ package worlds;
 import java.awt.Graphics;
 
 import tile.Tile;
+import utils.Utils;
 
 public class World {
 
 	private int width, height;
+	private int spawnX, spawnY;
 	private int[][] tiles;
 
 	public World(String path) {
@@ -34,13 +36,22 @@ public class World {
 	}
 
 	private void loadWorld(String path) {
-		width = 5;
-		height = 5;
-		tiles = new int[width][height];
-
-		for (int x = 0; x < width; x++)
-			for (int y = 0; y < height; y++) {
-				tiles[x][y] = 3;
+		String file = Utils.loadFileAString(path);
+		String[] tokens = file.split("\\s+");
+		
+		//World dimension
+		width = Utils.parseInt(tokens[0]);
+		height = Utils.parseInt(tokens[1]);
+		//Player spawn
+		spawnX= Utils.parseInt(tokens[2]);
+		spawnY = Utils.parseInt(tokens[3]);
+		//World code
+		tiles=new int[width][height];
+		for(int y=1;y<height;y++)
+			for(int x=1;x<width;x++){
+				tiles[x][y]=Utils.parseInt(tokens[(x+y*width)+4]);
 			}
+		
+
 	}
 }
